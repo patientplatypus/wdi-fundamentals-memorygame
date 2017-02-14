ea77e618e492bae824b7fc4def5346eb9e786b49
patientplatypus <pweyand@gmail.com>
2017-02-13 21:12:43 -0600

    Unit 10 exercise change

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+function createCards() {
+	var nextbook = document.getElementById('game-board');
+
+	for (i=0;i<4;i++){
+		var newCard = document.createElement("div");
+		newCard.setAttribute("class", "card");
+		nextbook.appendChild(newCard);
+	}
+}
+
+createCards();
+
+
+
+
+
+
